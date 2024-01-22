aaaabb8bfea732810b3d9a9cfe6189f3581beb6e
farfalan <9ae9alan@googlemail.com>
2024-01-22 15:36:24 +0100

    Aufgaben Order

---
+++ Aufgaben/sumInBinaryTree/SumInBinaryTree.py
@@ -0,0 +1,27 @@
+import math
+import sys
+
+try:
+    inputt = int(input("Gib eine ganze Zahl ein: "))
+    print("Du hast die ganze Zahl", inputt, "eingegeben.")
+except ValueError:
+    print("Fehler: Das ist keine ganze Zahl.")
+    sys.exit()
+    
+sum = 0
+for i in range(0,math.floor(math.log2(inputt)),1):
+    sum +=  math.floor(inputt/2**i)
+    print(f"node: {i} value  = {math.floor(inputt/2**i)}")
+
+sum =sum +1
+print(f"input = {inputt}")
+print(f"sum = {sum}")
+
+
+
+
+
+
+
+
+
